2b47227eae4be4f887f2e3822578af474bcfe589
HongChang <supcoder.lee@gmail.com>
2025-02-20 23:19:57 +0800

    Update test.kt

--- desktopApp/src/main/kotlin/test.kt
+++ desktopApp/src/main/kotlin/test.kt
@@ -1,3 +1,11 @@
-class Test{
-  
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class TestTest {
+
+    @Test
+    fun testExample() {
+        val testInstance = Test()
+        assertNotNull(testInstance)
+    }
 }
